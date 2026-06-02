5c524b9fa20c8f2a88ccdc6cc9edd45dcb3a834e
Voyage-for-the-ideal <wangyulin@mail.ustc.edu.cn>
2026-06-02 22:22:21 +0800

    fix(statesaver): preserve use card action target

--- STSStateSaver/src/main/java/savestate/actions/UseCardActionState.java
+++ STSStateSaver/src/main/java/savestate/actions/UseCardActionState.java
@@ -3,18 +3,21 @@ package savestate.actions;
 import basemod.ReflectionHacks;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
 import savestate.CardState;
 import savestate.fastobjects.actions.UpdateOnlyUseCardAction;
 
 public class UseCardActionState implements ActionState {
     private final CardState card;
     private final boolean exhaustCard;
+    private final Integer targetIndex;
 
     public UseCardActionState(UseCardAction action) {
         AbstractCard card = ReflectionHacks.getPrivate(action, UseCardAction.class, "targetCard");
 
         this.card = CardState.forCard(card);
         this.exhaustCard = action.exhaustCard;
+        this.targetIndex = ActionState.indexForCreature(action.target);
     }
 
     public UseCardActionState(UpdateOnlyUseCardAction action) {
@@ -23,14 +26,16 @@ public class UseCardActionState implements ActionState {
 
         this.card = CardState.forCard(card);
         this.exhaustCard = action.exhaustCard;
+        this.targetIndex = ActionState.indexForCreature(action.target);
     }
 
     @Override
     public UpdateOnlyUseCardAction loadAction() {
         AbstractCard resultCard = card.loadCard();
+        AbstractCreature target = ActionState
+                .creatureForIndex(targetIndex == null ? ActionState.NULL_INDEX : targetIndex);
 
-        // TODO: at some point the target here will matter
-        UpdateOnlyUseCardAction result = new UpdateOnlyUseCardAction(resultCard, null);
+        UpdateOnlyUseCardAction result = new UpdateOnlyUseCardAction(resultCard, target);
 
         result.exhaustCard = exhaustCard;
 
